86681273a9f09511bc7004a9ce37e7180cdf514e
COPPEL\jesusi.neoris <jibuenocastro@gmail.com>
2022-09-22 13:02:45 -0600

    Se sube archivo servletinitializer

--- src/main/java/com/proyecto/control/escolar/controlescolar/controllers/AlumnoController.java
+++ src/main/java/com/proyecto/control/escolar/controlescolar/controllers/AlumnoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.proyecto.control.escolar.controlescolar.components.alumnos.ResponseAl
 import com.proyecto.control.escolar.controlescolar.model.AlumnoModel;
 import com.proyecto.control.escolar.controlescolar.service.AlumnoService;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api")
 public class AlumnoController {
